7033dcac28c6e9cd57ba73edef02fe420f959c5b
FeritYILDIZ <feritstar@gmail.com>
2024-07-14 15:22:18 +0300

    CQRS Structure is added for Address Feature

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    public class RemoveAddressCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAddressCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address>? _repository;
+
+        public UpdateAddressCommandHandler(IRepository<Address>? repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAddressCommand updateAddressCommand)
+        {
+            var values = await _repository.GetByIdAsync(updateAddressCommand.AddressId);
+            values.Detail = updateAddressCommand.Detail;
+            values.City = updateAddressCommand.City;
+            values.District = updateAddressCommand.District;
+            values.UserId = updateAddressCommand.UserId;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
