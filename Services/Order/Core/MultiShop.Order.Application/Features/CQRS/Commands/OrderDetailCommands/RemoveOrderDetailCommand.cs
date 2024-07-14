171b70cfed3ce59ec1c82a9d8b07de43f5d5a4b3
FeritYILDIZ <feritstar@gmail.com>
2024-07-14 17:29:57 +0300

    OrderDetail CQRS and Domain features are added!

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/OrderDetailCommands/RemoveOrderDetailCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class RemoveOrderDetailCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveOrderDetailCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/OrderDeatilResults/GetOrderDetailByIdQueryResult.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/OrderDeatilResults/GetOrderDetailByIdQueryResult.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiShop.Order.Domain.Entities
+namespace MultiShop.Order.Application.Features.CQRS.Results.OrderDeatilResults
 {
-    public class OrderDetail
+    public class GetOrderDetailByIdQueryResult
     {
         public int OrderDetailId { get; set; }
         public string? ProductId { get; set; }
-        public string? ProductName { get; set;}
+        public string? ProductName { get; set; }
         public decimal ProductPrice { get; set; }
         public int ProductAmount { get; set; }
         public decimal ProductTotalPrice { get; set; }
         public int OrderingId { get; set; }
-        public Ordering? Ordering { get; set; }
     }
 }
